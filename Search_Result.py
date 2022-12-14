c2a45ef0be2e18e656e6dfa7e1ed76e8e49a9793
jhagspiel <j.hagspiel@student.tudelft.nl>
2022-12-14 16:41:40 +0100

    Added initial files.

---
+++ Search_Result.py
@@ -0,0 +1,11 @@
+
+class Search_Result:
+
+    def __init__(self, title, file_name, registration_date, description, contributors, tags):
+
+        self.title = title
+        self.file_name = file_name
+        self.registration_date = registration_date
+        self.description = description
+        self.contributors = contributors
+        self.tags = tags
